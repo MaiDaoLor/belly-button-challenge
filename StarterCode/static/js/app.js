92c035e03c91cf912b929d976419ad0e1e4d2e45
Maidao Lor <mdl06@yahoo.com>
2023-11-05 22:37:49 -0600

    app.js updatated

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -15,18 +15,25 @@ d3.json(url).then(function(result) {
         const id = ids[i]
         dropdownMenu.append("option").text(id)
     };
-
+    bar(ids[0])
 });
 
 // Create the Demographic Info
 
 
-// Create the bar chart
+// // Create the bar chart
 function bar(selectValue) {
+
+    // Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+    const samples = data.samples
+    const sample = samples.filter(element => element.id == selectValue) [0]
+    console.log(sample)
     
     let trace = [{
-        x:
-        y:
+        y: sample.otu_ids.slice(0,10).map(otu_id => "OTU " + otu_id).reverse(),
+        x: sample.sample_values.slice(0,10).reverse(),
+        text: sample.otu_labels.slice(0,10).reverse(),
         type: "bar",
         orientation: "h"
     }];
@@ -36,16 +43,47 @@ function bar(selectValue) {
         width: 800
     };
 
-    Plotly.newpot("bar", trace, layout)
+    Plotly.newPlot("bar", trace, layout)
+});
+
 }
 
 // Create the bubble chart
 
-// On change to the DOM, call getData()
-//d3.selectAll("#selDataset").on("change", getData);
+let sampleValues = Object.values(data.samples[0].sample_values);
+let otuIDs = Object.values(data.samples[0].otu_ids);
+let otuLables = Object.values(data.samples[0].otu_labels);
+//let metadata = data.metadata[0];
+
+function bubbleChart(selectValue) {
+    const bubble = [{
+        y: sampleValues
+        x: otuIDs
+        text: sample.otu_labels,
+        type: "bubble",
+        mode: "markers",
+        marker: {
+            size: sampleValues,
+            color: otuIDs,
+            colorscale: 'YlGnBu'
+        };
+        
+    }];
+
+    let layout2 = {
+        height: 400,
+        width: 1200,
+        margin: 
+        {t: 25, 
+        r: 25,
+        l: 25,
+        b: 25}
+    };
+
+    Plotly.newPlot("bubble", bubble, layout2)
+};
 
 // Function called by DOM changes
 function optionChanged(option) {
-//    let dropdownMenu = d3.select("")
-console.log(option);
+bar(option)
 }
